e569d2ea481ebf3794570a0b99c4ec0bbfd2c659
Asato846 <hatakeyama.asato01@gmail.com>
2025-08-22 13:35:59 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia'
+
+defineProps({
+    id: String,
+    blog : Object
+})
+
+const deleteConfirm = id=>{
+    // console.log(id)
+    Inertia.delete(`/inertia/${id}` ,{
+        onBefore: () => confirm('are you sure you want to delete?')
+    })
+
+}
+
+</script>
+
+<template>
+    {{ id }}<br>
+    {{ blog.title }}<br>
+    <button @click="deleteConfirm(blog.id)">delete</button>
+</template>
