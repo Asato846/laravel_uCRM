690290d0bab6fcde2bc356f43ffdc8e933b80a99
Asato846 <hatakeyama.asato01@gmail.com>
2025-08-26 16:49:00 +0900

    商品関連

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +15,22 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [            
+                'name' => 'cut',
+                'memo' => 'cut detail',
+                'price' => 6000
+            ],
+            [            
+                'name' => 'color',
+                'memo' => 'color detail',
+                'price' => 8000
+            ],
+            [            
+                'name' => 'perm and cut',
+                'memo' => 'detail',
+                'price' => 13000
+            ]
+        ]);
     }
 }
